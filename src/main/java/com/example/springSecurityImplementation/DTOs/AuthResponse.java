d1ea0c6d12b72381d10e4f0d7faeea941088bf6c
harshmishraskugal <harshmishra@skugal.com>
2025-08-25 13:59:29 +0530

    project structur

--- src/main/java/com/example/springSecurityImplementation/DTOs/AuthResponse.java
+++ src/main/java/com/example/springSecurityImplementation/DTOs/AuthResponse.java
@@ -1,5 +1,11 @@
 package com.example.springSecurityImplementation.DTOs;
 
 public class AuthResponse {
-
+    private final String token;
+    public AuthResponse(String token) {
+        this.token = token;
+    }
+    public String getToken() {
+        return token;
+    }
 }
